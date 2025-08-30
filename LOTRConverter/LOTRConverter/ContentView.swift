759b971fb4441740347a758757902159dceac7fd
Taha Fatih Kaya <tahafatihkaya@Taha-MacBook-Air.local>
2025-08-30 05:10:21 +0300

    App Created, First Upload

---
+++ LOTRConverter/LOTRConverter/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  LOTRConverter
+//
+//  Created by Taha Fatih Kaya on 29.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            // Background Image
+            
+            VStack {
+                // Image
+                
+                // Currency Exchange Text
+                
+                // Conversion Section
+                HStack {
+                    // Left Conversion Text
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Image
+                            // Text
+                        }
+                        // Text Field
+                    }
+                    // Equal Text
+                    
+                    // Right Conversion Text
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Text
+                            // Image 
+                        }
+                        // Text Field
+                    }
+                    
+                }
+                // Info Button
+                
+            }
+            
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter/LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Taha Fatih Kaya on 29.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
